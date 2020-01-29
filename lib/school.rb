0e400070f09df99990c12c07c25723e079f60919
Aaron Minyard <amnyrrd@gmail.com>
2020-01-29 23:11:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+  attr_accessor :roster
+  
+  def initialize
+  end
+  
+  def roster
+    @roster = {}
+  end
+end
+
+school = School.new("Bayside High School")
+school.roster
